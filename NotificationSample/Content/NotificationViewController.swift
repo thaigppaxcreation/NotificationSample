0ad4afdebc0d83528d25fcbb0892add85c41dcf9
Phan Gia Thai <thaigp@paxcreation.com>
2020-07-10 23:09:50 +0700

    Commit data

---
+++ NotificationSample/Content/NotificationViewController.swift
@@ -0,0 +1,52 @@
+//
+//  NotificationViewController.swift
+//  Content
+//
+//  Created by Lucas Goes Valle on 14/03/18.
+//  Copyright © 2018 Lucas Goes Valle. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        let content = notification.request.content
+        
+        if let fcm_options = content.userInfo["fcm_options"] as? [AnyHashable: Any] {
+            if let urlImageString = fcm_options["image"] as? String {
+                if let url = URL(string: urlImageString) {
+                    URLSession.downloadImage(atURL: url) { [weak self] (data, error) in
+                        if let _ = error {
+                            return
+                        }
+                        guard let data = data else {
+                            return
+                        }
+                        DispatchQueue.main.async {
+                            self?.imageView.image = UIImage(data: data)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension URLSession {
+    
+    class func downloadImage(atURL url: URL, withCompletionHandler completionHandler: @escaping (Data?, NSError?) -> Void) {
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, urlResponse, error) in
+            completionHandler(data, nil)
+        }
+        dataTask.resume()
+    }
+}
---
+++ NotificationSample/NotificationSample/AppDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  AppDelegate.swift
+//  NotificationSample
+//
+//  Created by Lucas Goes Valle on 13/03/18.
+//  Copyright © 2018 Lucas Goes Valle. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    let notificationDelegate = SampleNotificationDelegate()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        self.configureNotification()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+    func configureNotification() {
+        if #available(iOS 10.0, *) {
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options:[.badge, .alert, .sound]){ (granted, error) in }
+            center.delegate = notificationDelegate
+            let openAction = UNNotificationAction(identifier: "OpenNotification", title: NSLocalizedString("Abrir", comment: ""), options: UNNotificationActionOptions.foreground)
+            let deafultCategory = UNNotificationCategory(identifier: "CustomSamplePush", actions: [openAction], intentIdentifiers: [], options: [])
+            center.setNotificationCategories(Set([deafultCategory]))
+        } else {
+            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
+        }
+        UIApplication.shared.registerForRemoteNotifications()
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+        print("APNs device token: \(deviceTokenString)")
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("APNs registration failed: \(error)")
+    }
+}
+
